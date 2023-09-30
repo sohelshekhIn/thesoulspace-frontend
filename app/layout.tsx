6c4a6ac7e78468e9d76b7a7af1bc27f9408e1a10
Sohel Shekh <contact.sohelshekh@gmail.com>
2023-10-01 00:12:02 +0530

    Solved no id in next/Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,14 +110,14 @@ export default function RootLayout({
         src="https://www.googletagmanager.com/gtag/js?id=G-C671RSM83P"
         strategy="afterInteractive"
       />
-      <Script strategy="afterInteractive">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-C671RSM83P');`}
       </Script>
-      <Script strategy="afterInteractive">
+      <Script id="microsoft-clarity" strategy="afterInteractive">
         {` (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
